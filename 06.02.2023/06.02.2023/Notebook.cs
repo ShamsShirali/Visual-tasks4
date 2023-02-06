05be0b14086ddd09883034f1ce52d62a8e5c32ec
unknown <shamsshsh@code.edu.az>
2023-02-06 23:47:11 +0400

    06.02.23

---
+++ 06.02.2023/06.02.2023/Notebook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06._02._2023
+{
+    internal class Notebook
+    {
+       
+            public Notebook(string brand, string model)
+            {
+                Console.WriteLine("Notebook yaradildi:");
+                Brand = brand;
+                Model = model;
+            }
+
+
+            public string Brand;
+            public string Model;
+            public int Price;
+
+            public void ShowInfo()
+            {
+                Console.WriteLine($"Brand: {Brand} - Model: {Model} - Price: {Price}");
+
+
+
+            }
+        
+    }
+}
---
+++ 06.02.2023/06.02.2023/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _06._02._2023
+{
+  
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook ntb1 = new Notebook("Honor","Macbook")
+            {
+               
+                Price = 2900,
+            };
+
+            ntb1.ShowInfo();
+
+            Notebook ntb2 = new Notebook("Hp", "Laptop")
+            {
+
+                Price =3400,
+            };
+
+            ntb2.ShowInfo();
+
+            Notebook ntb3 = new Notebook("Apple", "Macbook")
+            {
+
+                Price = 6500,
+            };
+
+            ntb3.ShowInfo();
+
+            int[] notebook = {ntb1.Price,ntb2.Price,ntb3.Price};
+            int sum = 0;
+            int count = 0;
+            int result = 0;
+
+            for(int i=0;i<notebook.Length;i++)
+            {
+                sum += notebook[i];
+                count++;
+            }
+
+            result=sum/count;
+
+            Console.WriteLine("\nQiymet ortalamasi:");
+            Console.WriteLine(result);
+        }
+    }
+}
