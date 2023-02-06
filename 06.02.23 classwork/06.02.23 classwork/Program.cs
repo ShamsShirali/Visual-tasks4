3d782cd1873d496166d1753a3be71db83c378066
unknown <shamsshsh@code.edu.az>
2023-02-07 00:29:57 +0400

    06.02.23 classwork

---
+++ 06.02.23 classwork/06.02.23 classwork/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _06._02._23_classwork
+{
+    class Human{
+        public string Name;
+        public string Surname;
+        public byte Age;
+
+        public void GetFullname()
+        {
+            Console.WriteLine($"Name: {Name} - Surname: {Surname} - Age: {Age}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Human hmn1= new Human() {
+               Name="Shams",
+               Surname="Shirali",
+               Age=19
+           };
+            hmn1.GetFullname();
+
+            Student std1 = new Student(44)
+            {
+              Fullname="Sems Sireli",
+              GroupNo="P138",
+              
+            };
+            Console.WriteLine(std1.IsPassed()); 
+        }
+    }
+}
---
+++ 06.02.23 classwork/06.02.23 classwork/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace _06._02._23_classwork
+{
+    internal class Student
+    {
+        public Student(byte point)
+        {
+            Console.WriteLine("Student was created:");
+            Point = point;
+        }
+        public string Fullname;
+        public string GroupNo;
+        public byte Point;
+
+        public bool IsPassed() 
+        { 
+            if(Point>=65)
+            {
+                return true;
+            }
+            return false;
+        }  
+
+    }
+
+   
+}
