2572d2d4da9c8727b8e83be23cd74167af662d99
unknown <shamsshsh@code.edu.az>
2023-02-08 23:01:37 +0400

    08.02.23

---
+++ 08.02.23/08.02.23/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08._02._23
+{
+    internal class Employee
+    {
+        public string FullName;
+        protected double _salary;
+
+        public virtual double Salary
+        {
+           
+              get {
+
+                return _salary;
+              }
+              set
+              {
+                if (value>=300) {
+                    _salary = value;
+
+                }
+              }
+     }
+    }
+}
---
+++ 08.02.23/08.02.23/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _08._02._23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee()
+            {
+                FullName = "Shirali Shams",
+                Salary=10000
+            };
+
+            Console.WriteLine(emp1.Salary);
+
+            Engineer eng1 = new Engineer()
+            {
+                FullName = "Hacizade Elisafa",
+                Salary = 2000
+            };
+
+            Console.WriteLine(eng1.Salary);
+
+            Engineer eng2 = new Engineer()
+            {
+                FullName = "Shirali Shams",
+                Salary = 10000
+            };
+
+            Engineer eng3 = new Engineer()
+            {
+                FullName = "Eliyeva Ayten",
+                Salary = 1200
+            };
+
+            Console.WriteLine("Maas ortalamasi:");
+
+            double[] engineer = { eng1.Salary, eng2.Salary, eng3.Salary};
+
+            double sum = 0;
+            double result;
+
+            for(int i=0;i<engineer.Length;i++)
+            {
+                sum+= engineer[i];
+
+            }
+            result= sum/engineer.Length;
+
+            Console.WriteLine(result);
+        }
+    }
+}
