07a3f92ed19b9af285ccfeebe066dbacbaf9ead5
unknown <shamsshsh@code.edu.az>
2023-02-22 23:14:20 +0400

    22.02.23

---
+++ 22.02.23/22.02.23/Exam.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _22._02._23
+{
+    internal class Exam
+    {
+        public string Subject;
+        public DateTime StartedAt;
+        public DateTime FinishedAt;
+
+        List<Exam> Exams = new List<Exam>();
+
+        public int GetCountExams()
+        {
+            int count = 0;
+
+            foreach (var item in Exams)
+            {
+                var total = item.FinishedAt - item.StartedAt;
+
+                if (total.TotalHours < 2)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public void GetSubStar()
+        {
+            foreach(var item in Exams)
+            {
+                if (item.StartedAt >= DateTime.Now && item.StartedAt <=DateTime.Now.AddHours(24))
+                {
+                    Console.WriteLine(item.Subject);
+                    Console.WriteLine(item.StartedAt);
+                }
+            }
+        }
+
+        public DateTime GetStartData()
+        {
+            foreach(var item in Exams)
+            {
+                if (item.StartedAt >= DateTime.Today && item.StartedAt <=DateTime.Today.AddHours(24) )
+                {
+                    return item.StartedAt;
+                }
+            }
+            return DateTime.Now;
+        }
+
+        public bool LastYearProgram()
+        {
+            foreach(var item in Exams)
+            {
+                if (item.Subject == "Programming")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ 22.02.23/22.02.23/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _22._02._23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Exam> ExamList = new List<Exam>();
+            ExamList.Add(new Exam { Subject="Math",StartedAt=new DateTime(2023,2,23,9,0,0),FinishedAt=new DateTime(2023,2,23,13,0,0)});
+            ExamList.Add(new Exam { Subject = "Programming", StartedAt = new DateTime(2023, 2, 24, 10, 0, 0), FinishedAt = new DateTime(2023, 2, 24, 13, 0, 0) });
+            ExamList.Add(new Exam { Subject = "Physics", StartedAt = new DateTime(2023, 3, 23, 9, 0, 0), FinishedAt = new DateTime(2023, 3, 23, 13, 0, 0) });
+            ExamList.Add(new Exam { Subject = "Chemistry", StartedAt = new DateTime(2023, 4, 23, 9, 0, 0), FinishedAt = new DateTime(2023, 4, 23, 13, 0, 0) });
+            ExamList.Add(new Exam { Subject = "Algebra", StartedAt = new DateTime(2023, 5, 23, 9, 0, 0), FinishedAt = new DateTime(2023, 5, 23, 13, 0, 0) });
+            ExamList.Add(new Exam { Subject = "History", StartedAt = new DateTime(2023, 6, 23, 9, 0, 0), FinishedAt = new DateTime(2023, 6, 23, 13, 0, 0) });
+            ExamList.Add(new Exam { Subject = "English", StartedAt = new DateTime(2023, 7, 23, 9, 0, 0), FinishedAt = new DateTime(2023, 7, 23, 13, 0, 0) });
+            ExamList.Add(new Exam { Subject = "Analysis", StartedAt = new DateTime(2023, 8, 23, 9, 0, 0), FinishedAt = new DateTime(2023, 8, 23, 13, 0, 0) });
+            ExamList.Add(new Exam { Subject = "Computer architecture", StartedAt = new DateTime(2023, 9, 23, 9, 0, 0), FinishedAt = new DateTime(2023, 9, 23, 13, 0, 0) });
+            ExamList.Add(new Exam { Subject = "Philosophy", StartedAt = new DateTime(2023, 10, 23, 9, 0, 0), FinishedAt = new DateTime(2023, 10, 23, 13, 0, 0) });
+
+
+
+
+
+
+
+
+
+
+
+        }    
+    }
+}
